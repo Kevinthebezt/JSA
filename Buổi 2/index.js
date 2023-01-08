30cb48523162316ee78b400c3f5096506407ba97
Kevinthebezt <kevinnguyencao0108@gmail.com>
2023-01-08 11:54:41 +0700

    Buoi 2

---
+++ Buổi 2/index.js
@@ -0,0 +1,44 @@
+// var result = 0;
+// for (var i = 0; i <= 10; i++) {
+//     // console.log("Lập được " + i + " nè");
+//     result = result + i;
+
+// }
+// console.log(result);
+
+// var danhsachhocsinh = [1, 2, 3, "A", "B",];
+// console.log(danhsachhocsinh);
+// danhsachhocsinh.push("C","D");
+// danhsachhocsinh[1] = 0;
+// danhsachhocsinh.splice[0]
+
+// for (var i = 0; i < danhsachhocsinh.length; i++) {
+//     console.log(danhsachhocsinh[i]);
+// }
+
+// var Van = {
+//     age: 15,
+//     height: 174,
+//     location: "TPHCM"
+// }
+
+// Van.school = "BTX";
+
+// console.log(Van)
+
+// console.log(Van.school);
+
+// delete Van.school
+
+// console.log(Van)
+
+var myDog = {
+    name: "Ngáo",
+    legs: 4,
+    friends: ["everthing!"]
+};
+myDog.name = "Husky";
+myDog.color = "brown";
+delete myDog.friends;
+
+console.log(myDog)
